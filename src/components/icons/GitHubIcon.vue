5ad0c767d142f5d8633ce4f7ab3eff5c9999e0c0
LC <2583651948@qq.com>
2026-03-17 20:49:12 +0800

    feat(icons): add click event to open GitHub repo link
    - Added handleClick method in GitHubIcon component
    - Open GitHub repository in new tab on icon click
    - Made GitHub icon interactive with cursor pointer and click handler
    - Updated template to bind click event to handleClick function

--- src/components/icons/GitHubIcon.vue
+++ src/components/icons/GitHubIcon.vue
@@ -1,3 +1,12 @@
+<script setup lang="ts">
+const handleClick = () => {
+  window.open('https://github.com/wlitd/wlitd-web', '_blank')
+}
+</script>
+
 <template>
-  <div class="i-line-md:github-loop size-5 cursor-pointer" />
+  <div
+    class="i-line-md:github-loop size-5 cursor-pointer"
+    @click="handleClick"
+  />
 </template>
